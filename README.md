741e2045f23c8978343d23978cee347e03cbb9c3
fangjie05 <fangjie05@meituan.com>
2020-01-06 19:39:55 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# AndFurnace-炼丹炉
+
+## 1、Intro
+And(roid)Furnace：命名来源于太上老君的八卦炼丹炉。AndFurnace是Android Performace Manager(APM)利器，Android编译过程的炼丹炉，可以对编译过程中的res、arsc、
+AndroidManifest.xml，so，assets等文件做加工处理。
+
+
+## 2、Author
+
+@JayFang19933
---
+++ andfurnace/src/main/groovy/me/fangjie/andfurnace/AndFurnace.groovy
@@ -0,0 +1,13 @@
+package me.fangjie.andfurnace
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+public class AndFurnace implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println("hahahhahahah")
+
+    }
+}
